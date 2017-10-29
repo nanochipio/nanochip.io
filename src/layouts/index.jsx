b024a09dc5a6b74229a24436dbc9a03e9db9de45
Timo Horstschäfer <mail@timo-horstschaefer.de>
2017-10-29 18:20:48 +0100

    fix topnav links

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -29,12 +29,12 @@ const Nav = () => (
       <div className="topbar-right">
         <ul className="topbar-nav nav">
           <li className="nav-item">
-            <button className="nav-link active btn-link">Product <i className="fa fa-caret-down" /></button>
+            <Link className="nav-link active" href to="/">Product <i className="fa fa-caret-down" /></Link>
             <div className="nav-submenu">
-              <Link className="nav-link active" href to="#testimonials" data-scrollto="testimonials">Testimonials</Link>
-              <Link className="nav-link active" href to="#pricing" data-scrollto="pricing">Pricing</Link>
-              <Link className="nav-link active" href to="#features" data-scrollto="features">Features</Link>
-              <Link className="nav-link active" href to="#faq" data-scrollto="faq">FAQ</Link>
+              <Link className="nav-link active" href to="/#testimonials" data-scrollto="testimonials">Testimonials</Link>
+              <Link className="nav-link active" href to="/#pricing" data-scrollto="pricing">Pricing</Link>
+              <Link className="nav-link active" href to="/#features" data-scrollto="features">Features</Link>
+              <Link className="nav-link active" href to="/#faq" data-scrollto="faq">FAQ</Link>
             </div>
           </li>
           <li className="nav-item"><Link className="nav-link active" href to="/about">About</Link></li>
