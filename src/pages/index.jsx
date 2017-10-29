8bd1baa4e577fb34701f44d538320cbb98b6a956
Ben <ben@ledgy.com>
2017-10-29 16:58:28 +0100

    add mailchimp newsletter subscription url

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -203,7 +203,7 @@ const IndexPage = ({ data }: Object) => (
 
       <section className="section section-inverse py-40" style={{ backgroundColor: '#00b38d' }}>
         <div className="container">
-          <form className="form-glass row gap-y align-items-center" action="" method="post" target="_blank">
+          <form className="form-glass row gap-y align-items-center" action="https://ledgy.us16.list-manage.com/subscribe/post?u=d6181c123b4d20b2104c4652f&amp;id=c9cfbb11a6" method="post" target="_blank">
             <div className="col-12 col-md-3 offset-md-1 col-lg-2 offset-lg-2 py-1">
               <h4 className="mb-0 text-center">Stay up to date</h4>
             </div>
