20f1c0567ad18ff058131fc4aae4da293b57a2ce
Timo Horstschäfer <mail@timo-horstschaefer.de>
2018-03-14 11:50:20 +0100

    Trans tags in Features.jsx

--- src/components/FeaturesPage.jsx
+++ src/components/FeaturesPage.jsx
@@ -34,72 +34,83 @@ export default withI18n()(({ i18n, ...props }: Props) => (
 
           <Feature
             {...props}
-            name="Consistency"
-            title="Guaranteed Error-Free"
+            name={i18n.t`Consistency`}
+            title={i18n.t`Guaranteed Error-Free`}
             url="consistency"
           >
-            Did you know that many share registers are incomplete or
-            contain errors?
-            Building a share register with Excel or Word can easily lead to
-            inconsistencies.
-            Set up your shares with Ledgy
-            and be sure that all data is consistent at any time.
+            <Trans>
+              Did you know that many share registers are incomplete or
+              contain errors?
+              Building a share register with Excel or Word can easily lead to
+              inconsistencies.
+              Set up your shares with Ledgy
+              and be sure that all data is consistent at any time.
+            </Trans>
           </Feature>
 
           <Hr />
 
           <Feature
             {...props}
-            name="Financing Round Modeling"
+            name={i18n.t`Financing Round Modeling`}
             url="round-modeling"
             left
           >
-            What’s the stake percentage of the founders after a VC
-            invests $2,000,000 at a valuation of $7,500,000 taking into account
-            the 2 outstanding convertible loans and increasing the ESOP pool?<br />
-            Honestly, we don’t know. But Ledgy does.
+            <Trans>
+              What’s the stake percentage of the founders after a VC
+              invests $2,000,000 at a valuation of $7,500,000 taking into account
+              the 2 outstanding convertible loans and increasing the ESOP pool?<br />
+              Honestly, we don’t know. But Ledgy does.
+            </Trans>
           </Feature>
 
           <Hr />
 
           <Feature
             {...props}
-            name="Employee Stock Ownership Plans"
+            name={i18n.t`Employee Stock Ownership Plans`}
             url="esop"
           >
-            Let your employees be part of your company’s success.
-            Ledgy supports vested stock, options, inverse vesting, ESOP pools, and much more.<br />
-            Employees can even log into Ledgy and track their vested shares in real–time.
+            <Trans>
+              Let your employees be part of your company’s success.
+              Ledgy supports vested stock, options, inverse vesting,
+              ESOP pools, and much more.<br />
+              Employees can even log into Ledgy and track their vested shares in real–time.
+            </Trans>
           </Feature>
 
           <Hr />
 
           <Feature
             {...props}
-            name="Reporting & Documents"
+            name={i18n.t`Reporting & Documents`}
             url="reporting"
             left
           >
-            Oh, it’s tax season again and there are 50 holding confirmations
-            waiting to be sent to your shareholders.
-            This means pasting names into document templates,
-            computing the tax value of all shares… <br />
-            Don’t worry. Generate and send all important documents with a single click on Ledgy.
+            <Trans>
+              Oh, it’s tax season again and there are 50 holding confirmations
+              waiting to be sent to your shareholders.
+              This means pasting names into document templates,
+              computing the tax value of all shares … <br />
+              Don’t worry. Generate and send all important documents with a single click on Ledgy.
+            </Trans>
           </Feature>
 
           <Hr />
 
           <Feature
             {...props}
-            title="Investor Dashboard"
-            name="the Investor Dashboard"
+            title={i18n.t`Investor Dashboard`}
+            name={i18n.t`the Investor Dashboard`}
             url="investors"
           >
-            You are an investor and want to track your portfolio companies?
-            The founders are busy and the numbers they give you are different
-            from your own records?<br />
-            With their share register on Ledgy, there’s only a single source of truth.
-            Convince your companies and you will never miss anything regarding your investments.
+            <Trans>
+              You are an investor and want to track your portfolio companies?
+              The founders are busy and the numbers they give you are different
+              from your own records?<br />
+              With their share register on Ledgy, there’s only a single source of truth.
+              Convince your companies and you will never miss anything regarding your investments.
+            </Trans>
           </Feature>
 
         </div>
@@ -107,7 +118,7 @@ export default withI18n()(({ i18n, ...props }: Props) => (
 
 
       <p className="col-md-6 offset-md-5 text-right text-lighter small-2">
-        Illustrations by <a href="https://www.freepik.com/alekksall">Alekksall</a>
+        <Trans>Illustrations by</Trans> <a href="https://www.freepik.com/alekksall">Alekksall</a>
       </p>
 
     </main>
