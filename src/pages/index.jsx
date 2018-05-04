8dcedfffdf914d98aac2869f19a8d13a8b3b4506
Ben <ben@ledgy.com>
2018-05-04 12:18:23 +0200

    fix padding on index page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,9 +11,9 @@ import { demoUrl } from '../layouts/utils';
 const Header = ({ i18n, data, prefix }: Props) => (
   <header className="header bg-ledgy pb-0">
     <div className="container">
-      <div className="row align-items-center gap-y mt-5 mb-7">
+      <div className="row align-items-center gap-y mt-5 pb-7">
 
-        <div className="col-lg-6 ml-auto pb-6">
+        <div className="col-lg-6 ml-auto">
           <div className="mb-4 text-white">
             <h1><Trans>Build trust in your cap table</Trans></h1>
             <p>
