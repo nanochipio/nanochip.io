5d547a5c0ba6b7737d2a74db9c8373bfa42b457b
Tuan PM <tuanpm@live.com>
2018-05-10 16:55:30 +0700

    Refresh cache

--- gatsby-node.js
+++ gatsby-node.js
@@ -17,3 +17,19 @@ exports.onCreatePage = async ({ page, boundActionCreators }) => {
     resolve();
   });
 };
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  const timestamp = Date.now()
+  switch (stage) {
+    case 'build-javascript':
+      config.merge({
+        output: {
+          filename: `[name]-${timestamp}-[chunkhash].js`,
+          chunkFilename: `[name]-${timestamp}-[chunkhash].js`
+        }
+      })
+
+      break
+  }
+  return config
+}
