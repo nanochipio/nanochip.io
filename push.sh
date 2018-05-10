3bb11e6f7fc4f798a049964acf8fe6cf7866be09
Tuan PM <tuanpm@live.com>
2018-05-10 16:29:10 +0700

    Update CI

--- push.sh
+++ push.sh
@@ -2,7 +2,7 @@
 # Push HTML files to gh-pages automatically.
 
 # Fill this out with the correct org/repo
-ORG=nanochipvn
+ORG=nanochipio
 REPO=nanochip.io
 # This probably should match an email for one of your users.
 EMAIL=tuanpm@live.com
